ff24c30f02994d0acec814cd162837f19b7f3f97
manhunter7-2 <thomas.piquet7@gmail.com>
2023-06-19 16:21:46 +0200

    jokes + bootstrap + functs

--- fncts.js
+++ fncts.js
@@ -1,5 +1,12 @@
 const randomBtn = document.getElementById("randomJokeButton")
+const jokesPlace = document.getElementsByClassName("jokePlace")
 
 randomBtn.addEventListener('click', function(){
-    
+    console.log("Click !")
+    fetch("https://exam-back-tpiquet.onrender.com/random")
+    .then(response => response.json())
+    .then(data => {
+        jokesPlace[0].innerHTML = `<div class="joke-q">${data.question}</div>`
+        jokesPlace[0].innerHTML += `<div class="joke-a">${data.answer}</div>`
+    })
 })
